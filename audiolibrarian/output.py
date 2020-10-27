41e154b92c75799b485e9b2ba9872d6d27a399c9
Steve Jibson <steve@jibson.com>
2020-10-26 19:50:07 -0600

    Add comments, type-hints to output.Dots

--- audiolibrarian/output.py
+++ audiolibrarian/output.py
@@ -2,7 +2,15 @@ import sys
 
 
 class Dots:
-    def __init__(self, message):
+    """Context Manager that outputs a message, and dots...
+
+    Example:
+        with Dots("Please wait...") as d:
+            for _ in range(10):
+                time.sleep(1)  # or better, actually do some work here instead
+                d.dot()
+    """
+    def __init__(self, message: str):
         self._out(message)
 
     def __enter__(self):
@@ -11,10 +19,11 @@ class Dots:
     def __exit__(self, _, __, ___):
         self._out("\n")
 
-    def dot(self):
+    def dot(self) -> None:
+        """Output a dot."""
         self._out(".")
 
     @staticmethod
-    def _out(message):
+    def _out(message: str) -> None:
         sys.stdout.write(message)
         sys.stdout.flush()
