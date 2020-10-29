08ca3d66eb08d9c62f5432eaedc069a777655008
Steve Jibson <steve@jibson.com>
2020-10-29 12:32:30 -0600

    Print keys and values

--- tools/diff.py
+++ tools/diff.py
@@ -9,10 +9,10 @@ if __name__ == "__main__":
     filename1, filename2 = sys.argv[1:3]
 
     song1 = mutagen.File(filename1)
-    pprint.pp(sorted(song1.tags))
+    pprint.pp(dict(song1.tags))
 
     song2 = mutagen.File(filename2)
-    pprint.pp(sorted(song2.tags))
+    pprint.pp(dict(song2.tags))
 
     # pprint.pp(sorted(list(set(song1.tags) - set(song2.tags))))
     # pprint.pp(sorted(list(set(song2.tags) - set(song1.tags))))
