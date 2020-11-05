42c2ec08807bca0950ffe51dd8471ac0f0f755d7
Steve Jibson <steve@jibson.com>
2020-11-05 12:31:08 -0700

    Move version into __init__.py

--- setup.py
+++ setup.py
@@ -2,19 +2,26 @@ import glob
 import os
 from distutils.core import setup
 
-VERSION = '0.7.0'
+SCRIPTS = glob.glob(os.path.join("scripts", "*"))
 
-SCRIPTS = glob.glob(os.path.join('scripts', '*'))
 
-setup(
-    name='audiolibrarian',
-    version=VERSION,
-    packages=['audiolibrarian'],
-    url='https://bitbucket.org/toadstule/jib-audio/',
-    license='GNU General Public License v3.0',
-    author='Steve Jibson',
-    author_email='steve@jibson.com',
-    description='Audio library utilities',
-    scripts=SCRIPTS,
-    install_requires=[line.strip() for line in open("requirements_base.txt")]
-)
+def get_version():
+    with open("audiolibrarian/__init__.py") as pkg_init:
+        for line in pkg_init:
+            if line.startswith("__version__"):
+                return line.split('"')[1]
+
+
+if __name__ == "__main__":
+    setup(
+        name="audiolibrarian",
+        version=get_version(),
+        packages=["audiolibrarian"],
+        url="https://bitbucket.org/toadstule/jib-audio/",
+        license="GNU General Public License v3.0",
+        author="Steve Jibson",
+        author_email="steve@jibson.com",
+        description="Audio library utilities",
+        scripts=SCRIPTS,
+        install_requires=[line.strip() for line in open("requirements_base.txt")],
+    )
